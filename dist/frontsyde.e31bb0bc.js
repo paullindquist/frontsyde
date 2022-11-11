50de3a37ebe459a541035d9198416715e5aba716
Paul Lindquist <paul.lindquist@gmail.com>
2022-11-10 20:47:33 -0600

    Fix form

--- dist/frontsyde.e31bb0bc.js
+++ dist/frontsyde.e31bb0bc.js
@@ -118,7 +118,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
-
+document.forms['form'].onsubmit = function () {
+  document.getElementById("form_thanks").style.display = "block";
+  document.getElementById("contact_form").style.display = "none";
+};
 },{}],"../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62465" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49759" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ index.js
@@ -0,0 +1,4 @@
+document.forms['form'].onsubmit = function(){
+  document.getElementById("form_thanks").style.display = "block";
+  document.getElementById("contact_form").style.display = "none";
+}
